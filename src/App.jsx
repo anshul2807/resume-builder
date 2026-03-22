f73140fa25c1a356296fa1f9ee6d16521e240b97
Anshul singh <singhanshul2807@gmail.com>
2026-03-22 14:55:04 +0530

    fix: use HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ResumeProvider } from './context/ResumeContext';
 import { StyleProvider } from './context/StyleContext';
 import Builder from './pages/Builder';
